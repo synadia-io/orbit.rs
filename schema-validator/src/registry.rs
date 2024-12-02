4d5e67dc833e1fa65bc29a3222441b363634d927
Tomasz Pietrek <tomasz@nats.io>
2024-12-02 13:28:36 +0100

    Schema registry API binding WIP
    Signed-off-by: Tomasz Pietrek <tomasz@nats.io>

--- schema-validator/src/registry.rs
+++ schema-validator/src/registry.rs
@@ -21,13 +21,55 @@ pub struct AddResponse {
 }
 
 #[derive(Debug, Serialize)]
+pub struct GetRequest {
+    name: String,
+    version: u32,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct GetResponse {
+    schema: Schema,
+}
+
+#[derive(Debug, Serialize)]
+pub struct UpdateRequest {
+    name: String,
+    definition: Format,
+    description: String,
+    metadata: HashMap<String, String>,
+}
+
+#[derive(Debug, Serialize)]
+pub struct ListRequest {
+    filter: String,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct ListResponse {
+    schemas: Vec<Schema>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Schema {
+    name: String,
+    version: u32,
+    time: time::OffsetDateTime,
+    format: Format,
+    #[serde(rename = "compat_policy")]
+    compatibility_policy: CompatibilityPolicy,
+    description: String,
+    metadata: HashMap<String, String>,
+    deleted: bool,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
 pub enum Format {
     JsonSchema,
     Avro,
     Protobuf,
 }
 
-#[derive(Debug, Serialize)]
+#[derive(Debug, Serialize, Deserialize)]
 pub enum CompatibilityPolicy {
     None,
     Backward,
@@ -37,18 +79,11 @@ pub enum CompatibilityPolicy {
 
 pub struct Registry {
     client: async_nats::Client,
-    name: String,
 }
 
 impl Registry {
-    pub async fn new<A: ToServerAddrs>(
-        name: &str,
-        client: async_nats::Client,
-    ) -> Result<Self, async_nats::Error> {
-        Ok(Registry {
-            client,
-            name: name.to_string(),
-        })
+    pub async fn new<A: ToServerAddrs>(client: async_nats::Client) -> Self {
+        Registry { client }
     }
 
     pub async fn add(&self, request: AddRequest) -> Result<AddResponse, AddError> {
@@ -57,6 +92,33 @@ impl Registry {
             .client
             .request(format!("$R.v1.ADD.{}", request.name), request_bytes)
             .await?;
+        let result = serde_json::from_slice(&response.payload)?;
+        Ok(result)
+    }
+
+    pub async fn get(&self, request: GetRequest) -> Result<GetResponse, GetError> {
+        let request_bytes = serde_json::to_vec(&request).map(Bytes::from)?;
+        let response = self
+            .client
+            .request(format!("$R.v1.GET.{}", request.name), request_bytes)
+            .await?;
+        let result = serde_json::from_slice(&response.payload)?;
+        Ok(result)
+    }
+
+    pub async fn update(&self, request: AddRequest) -> Result<AddResponse, AddError> {
+        let request_bytes = serde_json::to_vec(&request).map(Bytes::from)?;
+        let response = self
+            .client
+            .request(format!("$R.v1.UPDATE.{}", request.name), request_bytes)
+            .await?;
+        let result = serde_json::from_slice(&response.payload)?;
+        Ok(result)
+    }
+
+    pub async fn list(&self, request: ListRequest) -> Result<ListResponse, GetError> {
+        let request_bytes = serde_json::to_vec(&request).map(Bytes::from)?;
+        let response = self.client.request("$R.v1.LIST", request_bytes).await?;
         let result = serde_json::from_slice(&response.payload)?;
         Ok(result)
     }
@@ -98,3 +160,41 @@ impl From<serde_json::Error> for AddError {
         AddError::with_source(AddErrorKind::Other, err)
     }
 }
+
+pub type GetError = Error<GetErrorKind>;
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum GetErrorKind {
+    SchemaRegistryNotFound,
+    TimedOut,
+    Other,
+}
+
+impl Display for GetErrorKind {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            GetErrorKind::SchemaRegistryNotFound => write!(f, "Schema registry not found"),
+            GetErrorKind::TimedOut => write!(f, "Timed out"),
+            GetErrorKind::Other => write!(f, "Other error"),
+        }
+    }
+}
+
+impl From<async_nats::RequestError> for GetError {
+    fn from(err: async_nats::RequestError) -> Self {
+        match err.kind() {
+            async_nats::RequestErrorKind::TimedOut => GetError::new(GetErrorKind::TimedOut),
+            async_nats::RequestErrorKind::NoResponders => {
+                GetError::new(GetErrorKind::SchemaRegistryNotFound)
+            }
+            async_nats::RequestErrorKind::Other => GetError::with_source(GetErrorKind::Other, err),
+        }
+    }
+}
+impl From<serde_json::Error> for GetError {
+    fn from(err: serde_json::Error) -> Self {
+        GetError::with_source(GetErrorKind::Other, err)
+    }
+}
+
+pub type UpdateError = GetError;
